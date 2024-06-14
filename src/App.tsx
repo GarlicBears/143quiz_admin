11cba6d1da1b3299f4c322bf1275932cd26fa058
HyunJin, Lee <nagi5924@gmail.com>
2024-06-14 15:24:17 +0900

    [GB-27] design : 공통 레이아웃, SideMenu, 에러 페이지

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Layout from './Components/common/Layout';
 import Dashboard from './Pages/Dashboard';
+import Login from './Pages/Login';
+import Error from './Pages/Error';
 
 function App() {
   return (
@@ -10,6 +12,9 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Dashboard />} />
         </Route>
+
+        <Route path="/login" element={<Login />} />
+        <Route path={'*'} element={<Error />} />
       </Routes>
     </Router>
   );
