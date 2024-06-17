408a4469e812111374e651cb297583d4306820ae
Hyun Jin, Lee <76608100+lhj5924@users.noreply.github.com>
2024-06-17 15:35:26 +0900

    [GB-27] design : 공통 레이아웃, SideMenu, 에러 페이지 (#1)
    * [GB-27] design : 공통 레이아웃, SideMenu, 에러 페이지

--- src/Pages/Dashboard.tsx
+++ src/Pages/Dashboard.tsx
@@ -1,10 +1,94 @@
 import React from 'react';
+import { SimpleGrid, Text, Flex, GridItem } from '@chakra-ui/react';
 
 const Dashboard = () => {
   return (
-    <div>
-      <h1>Dashboard</h1>
-    </div>
+    <SimpleGrid columns={5} spacing={4}>
+      <GridItem colSpan={1}>
+        <Flex
+          justifyContent="center"
+          alignItems="center"
+          flexDirection="column"
+          py={10}
+          px={6}
+          bgColor={'#f7f7f7'}
+        >
+          <Text fontSize="2xl" mb={4}>
+            1
+          </Text>
+        </Flex>
+      </GridItem>
+      <GridItem colSpan={4}>
+        <Flex
+          justifyContent="center"
+          alignItems="center"
+          flexDirection="column"
+          py={10}
+          px={6}
+          bgColor={'#f7f7f7'}
+        >
+          <Text fontSize="2xl" mb={4}>
+            4
+          </Text>
+        </Flex>
+      </GridItem>
+      <GridItem colSpan={3}>
+        <Flex
+          justifyContent="center"
+          alignItems="center"
+          flexDirection="column"
+          py={10}
+          px={6}
+          bgColor={'#f7f7f7'}
+        >
+          <Text fontSize="2xl" mb={4}>
+            3
+          </Text>
+        </Flex>
+      </GridItem>
+      <GridItem colSpan={2}>
+        <Flex
+          justifyContent="center"
+          alignItems="center"
+          flexDirection="column"
+          py={10}
+          px={6}
+          bgColor={'#f7f7f7'}
+        >
+          <Text fontSize="2xl" mb={4}>
+            2
+          </Text>
+        </Flex>
+      </GridItem>
+      <GridItem colSpan={2}>
+        <Flex
+          justifyContent="center"
+          alignItems="center"
+          flexDirection="column"
+          py={10}
+          px={6}
+          bgColor={'#f7f7f7'}
+        >
+          <Text fontSize="2xl" mb={4}>
+            2
+          </Text>
+        </Flex>
+      </GridItem>
+      <GridItem colSpan={3}>
+        <Flex
+          justifyContent="center"
+          alignItems="center"
+          flexDirection="column"
+          py={10}
+          px={6}
+          bgColor={'#f7f7f7'}
+        >
+          <Text fontSize="2xl" mb={4}>
+            3
+          </Text>
+        </Flex>
+      </GridItem>
+    </SimpleGrid>
   );
 };
 export default Dashboard;
---
+++ src/Pages/Login.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import CommonHeader from '../Components/common/CommonHeader';
+
+const Login = () => {
+  return (
+    <>
+      <CommonHeader />
+      Login
+    </>
+  );
+};
+
+export default Login;
