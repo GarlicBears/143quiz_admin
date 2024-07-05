54f79c6944c2d7f70a2a34d67e5d0a01984d7927
HyunJin, Lee <nagi5924@gmail.com>
2024-07-05 10:13:01 +0900

    [GB-79] feat : 정렬 방식 서버에서 받아오는 방식으로 변경

--- src/API/axiosInstance.ts
+++ src/API/axiosInstance.ts
@@ -6,7 +6,7 @@ import axios, {
 } from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8080/api',
+  baseURL: 'http://localhost:8080/api',
   timeout: 10000,
   withCredentials: true,
 });
