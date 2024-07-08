582f9af039a453d315e750e7a286d09fa78f6f30
Hyun Jin, Lee <76608100+lhj5924@users.noreply.github.com>
2024-07-08 20:48:05 +0900

    [GB-108] feat : deploy setting (#25)

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ import PrivateRoute from './Components/PrivateRoute';
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.REACT_APP_WS_URL}>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="/" element={<PrivateRoute />}>
