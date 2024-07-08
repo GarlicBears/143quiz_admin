2afe260226af68d12cc9aff37d5aec082fe2e445
HyunJin, Lee <nagi5924@gmail.com>
2024-07-08 20:34:32 +0900

    [GB-108] feat : deploy setting

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -8,7 +8,7 @@ import Cookies from 'js-cookie';
 
 // axios 인스턴스 생성
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8080/api',
+  baseURL: process.env.REACT_APP_API_URL || 'https://garlicbears.com/api',
   timeout: 5000,
   withCredentials: true,
 });
